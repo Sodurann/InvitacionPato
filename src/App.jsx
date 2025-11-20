0521bf864b68cc6665fde6c8043431fb7139a9f9
Sofia Duran <sofiduran05.2@gmail.com>
2025-11-20 13:07:30 -0300

    se arreglo app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/welcomePage";
-import InvitationPage from "../pages/invitationPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/invitacion" element={<InvitationPage />} />
       </Routes>
     </BrowserRouter>
   );
